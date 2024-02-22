6de29e7e7fd18638f79eb456e3a1ba9cc24c0506
Obua Stella <stellaobua123@gmail.com>
2024-02-22 16:10:16 +0100

    Added styles for create component

--- app/src/Create.jsx
+++ app/src/Create.jsx
@@ -1,3 +1,44 @@
+import { Link } from "react-router-dom";
+
 export const Create = () => {
-  return <div>Create</div>;
+  return (
+    <div className="d-flex w-100 vh-100 justify-content-center align-items-center bg-light">
+      <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
+        <h1>Add a User</h1>
+        <form action="">
+          <div className="mb-2">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Name"
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter Email"
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="name">Phone Number:</label>
+            <input
+              type="tel"
+              className="form-control"
+              placeholder="Enter Phone Number"
+            />
+          </div>
+          <button type="submit" className="btn btn-success">
+            Submit
+          </button>
+
+          <Link to="/" className="btn btn-primary ms-3">
+            Back
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 };
