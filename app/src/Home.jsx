3a664e517dc3e5a7b2cbfd481ee81a774914165a
Obua Stella <stellaobua123@gmail.com>
2024-02-22 15:42:20 +0100

    Displaying records

--- app/src/Home.jsx
+++ app/src/Home.jsx
@@ -1,16 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 export const Home = () => {
+  const [data, setData] = useState([]);
   useEffect(() => {
     axios
       .get("http://localhost:3000/users")
-      .then((res) => console.log(res))
+      .then((res) => setData(res.data))
       .catch((err) => console.log(err));
   }, []);
   return (
     <div className="d-flex flex-column justify-content-center align-items-center bg-dark vh-100">
       <h1 className="text-light">List of Users</h1>
-      <div className="w-75 rounded bg-white border shadow p-4"></div>
+      <div className="w-75 rounded bg-white border shadow p-4">
+        {/* Display data in table */}
+        <table className="table table-stipend">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((d, i) => (
+              <tr key={i}>
+                <td>{d.id}</td>
+                <td>{d.name}</td>
+                <td>{d.email}</td>
+                <td>{d.phone}</td>
+                <td>
+                  <button className="btn btn-sm btn-primary me-2">Edit</button>
+                  <button className="btn btn-sm btn-danger">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
